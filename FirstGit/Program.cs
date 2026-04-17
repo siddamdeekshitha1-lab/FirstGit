53552dfbfff0b67b99564ce74df519f18fd59659
siddamdeekshitha1-lab <siddamdeekshitha1@gmail.com>
2026-04-17 09:47:09 +0530

    Update Program.cs

--- FirstGit/Program.cs
+++ FirstGit/Program.cs
@@ -11,6 +11,7 @@ namespace FirstGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WrtieLine("New Line Added");
         }
     }
 }
